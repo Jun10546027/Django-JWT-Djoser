75fa021e7e2c14c6494e4a74637fdb8c511d3a87
10546027Jun <10546027@ntub.edu.tw>
2019-06-04 23:55:28 +0800

    設定search,ordering,filter功能

--- test_token/models.py
+++ test_token/models.py
@@ -15,6 +15,7 @@ class User(AbstractUser):
 class Coupon(models.Model):
     # user = models.ForeignKey(User,on_delete=models.CASCADE)
     coupon_id = models.CharField(max_length=20, primary_key=True)
+    coupon_price = models.IntegerField()
     coupon_title = models.CharField(max_length=150)
     coupon_class = models.CharField(max_length=20)
     coupon_content = models.CharField(max_length=200)
