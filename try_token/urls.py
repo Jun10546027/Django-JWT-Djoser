4488baeadbcc413f51765f11d9822188b5460999
10546027Jun <10546027@ntub.edu.tw>
2019-06-10 01:43:56 +0800

    'media的設置'

--- try_token/urls.py
+++ try_token/urls.py
@@ -6,6 +6,9 @@ from rest_framework_jwt.views import refresh_jwt_token
 from rest_framework_jwt.views import verify_jwt_token
 from rest_framework.routers import DefaultRouter
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from test_token.views import MusicViewSet,CouponViewSet,UserViewSet,UserFavViewset
 
 router = DefaultRouter()
@@ -23,4 +26,4 @@ urlpatterns = [
     path('api-token-refresh/', refresh_jwt_token),
     path('api-token-verify/', verify_jwt_token),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
